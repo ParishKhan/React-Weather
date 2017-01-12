58c8562e2920944163f436b9f5e385b4db724aa5
PariSh KhAn <xttrra@gmail.com>
2017-01-12 21:31:27 +0600

    Style Nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,16 +1,33 @@
 import React, {Component} from 'react';
 import {Link, IndexLink} from 'react-router';
 
-var Nav = (props) => {
-    return (
-        <div>
-            <ul>
-                <li><IndexLink activeClassName="active" to="/">Home</IndexLink></li>
-                <li><Link activeClassName="active" to="/About">About</Link></li>
-                <li><Link activeClassName="active" to="/Examples">Examples</Link></li>
-            </ul>
-        </div>
-    );
+class Nav extends Component {
+    onSearch(e) {
+        e.preventDefault()
+        alert('Not yet wired up')
+    }
+    render() {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="dropdown menu" data-dropdown-menu>
+                        <li className="menu-text">React Weather App</li>
+                        <li><IndexLink activeClassName="active" to="/">Home</IndexLink></li>
+                        <li><Link activeClassName="active" to="/About">About</Link></li>
+                        <li><Link activeClassName="active" to="/Examples">Examples</Link></li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li><input type="search" placeholder="Search" /></li>
+                            <li><button type="button" className="button">Search Weather</button></li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Nav;
